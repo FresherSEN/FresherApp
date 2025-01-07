4e7b411b4c8d6b25218b8858d21d3fef148358cd
Yaveen! <94953863+Yaveen123@users.noreply.github.com>
2025-01-08 07:57:11 +1100

    Tried again.

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -14,10 +14,12 @@ async function handleLogin() {
     console.log("helo");
     account.createOAuth2Session(
         Appwrite.OAuthProvider.Google,
-        'http://freshersen.github.io/FresherApp/',
+        'https://freshersen.github.io/FresherApp/',
         'https://freshersen.github.io/FresherApp/'
     );
 }
+
+
 
 async function getUser() {
     try {
@@ -33,10 +35,22 @@ function renderLoginScreen() {
 }
 
 async function renderProfileScreen(user) {
-    console.log(user);
     document.getElementById("temp-login-status").innerHTML = user.name;
     profileScreen.style.display = ''; //‘Blackus’ (2015) Remove Style on Element, Accessed 6 Jan 2025 https://stackoverflow.com/questions/18691655/remove-style-on-element 
+    loginScreen.style.display = 'none';
+
+    //POST the data to the server
+    fetch('/api/logUserIn', {
+        method: "POST",
+        body: JSON.stringify(user), // Send the user data from google
+        headers: { //Set the endpoint to support this
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    })
+    .then((response) => response.json())
+    //.then((json) => console.log(json));
 }
+
 async function handleLogout() {
     account.deleteSession('current');
     profileScreen.style.display = 'none';
@@ -44,3 +58,4 @@ async function handleLogout() {
 }
 
 getUser();
+
